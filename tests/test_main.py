0745cbf6c0e6f1067792f48d5563dcc589fc7cea
dodir <mokronosovmw@gmail.com>
2023-10-24 06:34:44 +0500

    lesson 3

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,15 @@ def test_main():
 
 def test_2():
     assert 2 == 2
+
+
+def test_sum():
+    x = 1
+    y = 2
+    assert x + y == 3
+
+
+def test_division():
+    x = 1
+    y = 2
+    assert x / y == 0.5
