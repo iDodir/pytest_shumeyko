33280dc5eb5cfd1a4db9f2e1b81a27eefac6de89
dodir <mokronosovmw@gmail.com>
2023-10-24 07:05:28 +0500

    lesson 5: exceptions

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,32 @@
+from contextlib import nullcontext as does_not_raise
+
 import pytest
 
 from src.main import Calculator
 
 
-@pytest.mark.parametrize(
-    ["x", "y", "res"],
-    [
-        (1, 2, 0.5),
-        (5, -1, -5),
-    ]
-)
-def test_divide(x, y, res):
-    assert Calculator().divide(x, y) == res
+class TestCalculator:
+    @pytest.mark.parametrize(
+        ["x", "y", "res", "expectation"],
+        [
+            (1, 2, 0.5, does_not_raise()),
+            (5, -1, -5, does_not_raise()),
+            (5, "-1", -5, pytest.raises(TypeError)),
+            (5, 0, float("inf"), pytest.raises(ZeroDivisionError)),
+        ]
+    )
+    def test_divide(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().divide(x, y) == res
 
-
-@pytest.mark.parametrize(
-    ["x", "y", "res"],
-    [
-        (1, 2, 3),
-        (5, -1, 4),
-    ]
-)
-def test_add(x, y, res):
-    assert Calculator().add(x, y) == res
+    @pytest.mark.parametrize(
+        ["x", "y", "res", "expectation"],
+        [
+            (1, 2, 3, does_not_raise()),
+            (5, -1, 4, does_not_raise()),
+            (5, "-1", 4, pytest.raises(TypeError)),
+        ]
+    )
+    def test_add(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().add(x, y) == res
