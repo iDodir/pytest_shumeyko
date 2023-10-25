0cfd7329dbd263d8b33b6c7a8aae22f95aafd345
dodir <mokronosovmw@gmail.com>
2023-10-25 07:04:29 +0500

    lesson 6_1: fixture basics

--- tests/test_list_1.py
+++ tests/test_list_1.py
@@ -2,24 +2,12 @@ import pytest
 
 from src.candies.schemas import CandySchema
 from src.candies.service import CandiesService
-from src.db import Base, engine
-from src.candies.models import Candies
-
-from src.config import settings
-
-
-@pytest.fixture(scope="session", autouse=True)
-def setup_db():
-    print(f"{settings.DB_NAME=}")
-    assert settings.MODE == "TEST"
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
 
 
 @pytest.fixture
 def candies():
     candies = [
-        CandySchema(title="candy1", owner="gg"),
+        CandySchema(title="candy1", owner="dodir"),
         CandySchema(title="candy2", state="eaten"),
         CandySchema(title="candy3", state="half"),
     ]
