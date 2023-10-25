0e715c35351523dada784deb2b6f687ecb0bcfb0
dodir <mokronosovmw@gmail.com>
2023-10-25 07:26:00 +0500

    lesson 8: conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.config import settings
+from src.db import Base, engine
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_db():
+    print(f"{settings.DB_NAME=}")
+    assert settings.MODE == "TEST"
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
