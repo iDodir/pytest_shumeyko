df03c39a0e62f946fc8a5183cd2dc4be5ff77be6
dodir <mokronosovmw@gmail.com>
2023-10-25 07:07:29 +0500

    lesson 6_2: fixtures - database setup, scopes

--- tests/test_list_1.py
+++ tests/test_list_1.py
@@ -2,6 +2,16 @@ import pytest
 
 from src.candies.schemas import CandySchema
 from src.candies.service import CandiesService
+from src.config import settings
+from src.db import Base, engine
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_db():
+    print(f"{settings.DB_NAME=}")
+    assert settings.MODE == "TEST"
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
 
 
 @pytest.fixture
