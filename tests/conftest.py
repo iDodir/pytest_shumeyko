5cf064c4d2c66c5adbca4f05e1011bd246cc0b9b
dodir <mokronosovmw@gmail.com>
2023-10-25 07:51:33 +0500

    lesson 9: slow tests, test skipping, addoption

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from src.config import settings
@@ -10,3 +12,43 @@ def setup_db():
     assert settings.MODE == "TEST"
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        default="chrome",
+        choices=("chrome", "firefox"),
+    )
+    parser.addoption(
+        "--run-slow",
+        default="true",
+        choices=("true", "false"),
+    )
+
+
+@pytest.fixture
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+def test_1(browser):
+    print(browser)
+
+
+# @pytest.mark.skip(reason="Slow test")
+@pytest.mark.skipif('config.getoption("--run-slow") == "false"')
+def test_slow():
+    time.sleep(3)
+
+
+def test_fast():
+    pass
+
+
+def test_fast_2():
+    time.sleep(2)
+
+
+def test_fast_1():
+    time.sleep(1)
